2289e07d8637ed2d794dba97813b538e6b5f16ed
joelm-g <joelmar.grecia@wvsu.edu.ph>
2026-01-21 15:36:05 +0800

    feat: add grecia's files to main

---
+++ BSCS 3B/JOELMAR GRECIA/regex-eliza.py
@@ -0,0 +1,7 @@
+import re
+
+paragraph = """Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do.  Once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, "and what is the use of a book," thought Alice, "without pictures or conversations?"""
+
+pattern = r"[A-Z][a-zA-Z]*"
+matches = re.findall(pattern, paragraph)
+print(matches)
